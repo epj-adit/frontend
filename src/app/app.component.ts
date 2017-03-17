60df2af04b20e505e2ab1cba0043010ce4a49f3c
Muriele Trentini <muriele@trentini.ch>
2017-03-17 19:40:33 +0100

    add basic html and scss and remove unused components from boilerplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
+
 /**
  * Styles required here are common for all components (SASS/SCSS versions of normalize.css and flexboxgrid),
  * so encapsulation is not used. Other components have their styles scoped with `ViewEncapsulation.Emulated`.
  */
 @Component({
     selector: 'adit-app',
-    templateUrl: './app.html',
-    styleUrls: ['./app.scss'],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
     encapsulation: ViewEncapsulation.None,
     providers: []
 })
-export class AppComponent {}
+export class AppComponent {
+
+}
