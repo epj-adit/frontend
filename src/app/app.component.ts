380c3fc4065ab4ea56b3b3a39ff460981e0ab3a4
Muriele Trentini <muriele@trentini.ch>
2017-03-27 14:11:46 +0200

    add i19n support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 import { Advertisement } from './data-classes/advertisement';
 import { AdvertisementService } from './advertisements/advertisement.service';
@@ -7,19 +8,27 @@ import { AdvertisementService } from './advertisements/advertisement.service';
  * so encapsulation is not used. Other components have their styles scoped with `ViewEncapsulation.Emulated`.
  */
 @Component({
-    selector: 'adit-app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    encapsulation: ViewEncapsulation.None,
-    providers: []
+  selector: 'adit-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  providers: []
 })
-export class AppComponent implements OnInit{
-    advertisements: Advertisement[] = [];
+export class AppComponent implements OnInit {
+  advertisements: Advertisement[] = [];
 
-    constructor(private advertisementService: AdvertisementService) { }
+  constructor(private advertisementService: AdvertisementService, private translate: TranslateService) {
+    translate.addLangs(['de', 'en']);
+    translate.setDefaultLang('de');
+    translate.use('de');
+  }
 
-    ngOnInit(): void {
-        this.advertisementService.getAdvertisements()
-            .then(advertisements => this.advertisements = advertisements);
-    }
+  changeLang(lang: string) {
+    this.translate.use(lang);
+  }
+
+  ngOnInit(): void {
+    this.advertisementService.getAdvertisements()
+      .then(advertisements => this.advertisements = advertisements);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule, JsonpModule } from '@angular/http';
+import { HttpModule, Http, JsonpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
 
 import { routing, appRoutingProviders }  from './app.routing';
 import { InMemoryAdvertisementService } from './mock-data/in-memory-advertisement.service';
@@ -12,22 +14,33 @@ import { AppComponent } from './app.component';
 import { AdvertisementSearchComponent } from './search/advertisement-search.component';
 import { AdvertisementComponent } from './advertisements/advertisements.component';
 
+export function HttpLoaderFactory(http: Http) {
+  return new TranslateHttpLoader(http);
+}
+
 @NgModule({
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule,
-        JsonpModule,
-        InMemoryWebApiModule.forRoot(InMemoryAdvertisementService),
-        routing
-    ],
-    declarations: [
-        AppComponent,
-        AdvertisementSearchComponent,
-        AdvertisementComponent
-    ],
-    providers: [ appRoutingProviders, AdvertisementService ],
-    bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
+    InMemoryWebApiModule.forRoot(InMemoryAdvertisementService, {passThruUnknownUrl: true}),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [Http]
+      }
+    }),
+    routing
+  ],
+  declarations: [
+    AppComponent,
+    AdvertisementSearchComponent,
+    AdvertisementComponent
+  ],
+  providers: [appRoutingProviders, AdvertisementService],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
