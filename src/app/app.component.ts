09c8e822249bb01f0cd34609bbee6bbc37105978
Muriele Trentini <muriele@trentini.ch>
2017-04-11 11:17:43 +0200

    remove unused service access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, ViewEncapsulation, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 import { Advertisement } from './data-classes/advertisement';
-import { AdvertisementService } from './_services/advertisement.service';
 /**
  * Styles required here are common for all components (SASS/SCSS versions of normalize.css and flexboxgrid),
  * so encapsulation is not used. Other components have their styles scoped with `ViewEncapsulation.Emulated`.
@@ -14,10 +13,10 @@ import { AdvertisementService } from './_services/advertisement.service';
   encapsulation: ViewEncapsulation.None,
   providers: []
 })
-export class AppComponent implements OnInit {
+export class AppComponent{
   advertisements: Advertisement[] = [];
 
-  constructor(private advertisementService: AdvertisementService, private translate: TranslateService) {
+  constructor(private translate: TranslateService) {
     translate.addLangs(['de', 'en']);
     translate.setDefaultLang('de');
     translate.use('de');
@@ -27,8 +26,4 @@ export class AppComponent implements OnInit {
     this.translate.use(lang);
   }
 
-  ngOnInit(): void {
-    this.advertisementService.getAdvertisements()
-      .then(advertisements => this.advertisements = advertisements);
-  }
 }
