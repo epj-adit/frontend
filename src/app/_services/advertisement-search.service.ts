754710f948fd17ca51df2f5a304c53fe7d84f425
Muriele Trentini <muriele@trentini.ch>
2017-04-26 07:19:18 +0200

    add spaces in import->tslint

--- src/app/_services/advertisement-search.service.ts
+++ src/app/_services/advertisement-search.service.ts
@@ -6,16 +6,16 @@ import 'rxjs/add/operator/map';
 
 import { AppSettings } from '../app.settings';
 import { Advertisement }           from '../data-classes/advertisement';
-import {Category} from "../data-classes/category";
-import {Tag} from "../data-classes/tag";
-import {SearchProposal} from "../data-classes/search-proposal";
+import { Category } from "../data-classes/category";
+import { Tag } from "../data-classes/tag";
+import { SearchProposal } from "../data-classes/search-proposal";
 
 @Injectable()
 export class AdvertisementSearchService {
 
-  private apiUrlAdvertisements = AppSettings.API_ENDPOINT+'/advertisements';
-  private apiUrlTags= AppSettings.API_ENDPOINT+'/tags';
-  private apiUrlCategories = AppSettings.API_ENDPOINT+'/categories';
+  private apiUrlAdvertisements = AppSettings.API_ENDPOINT + '/advertisements';
+  private apiUrlTags = AppSettings.API_ENDPOINT + '/tags';
+  private apiUrlCategories = AppSettings.API_ENDPOINT + '/categories';
 
   constructor(private http: Http) {
   }
@@ -26,7 +26,7 @@ export class AdvertisementSearchService {
     let tagsRequest = this.http.get(`${this.apiUrlTags}/?name=${term}`).map(res => res.json() as Tag[]);
     let categoriesRequest = this.http.get(`${this.apiUrlCategories}/?name=${term}`).map(res => res.json() as Category[]);
 
-     return Observable.forkJoin([advertisementsRequest, tagsRequest, categoriesRequest]);
+    return Observable.forkJoin([advertisementsRequest, tagsRequest, categoriesRequest]);
 
   }
 
