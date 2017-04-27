268468c4024abba6c6ba093fc7f977e62d2fd8ee
Muriele Trentini <muriele@trentini.ch>
2017-04-27 10:44:42 +0200

    change api endpoint back to develop

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,6 +1,6 @@
 export class AppSettings{
     // develop
-    //public static API_ENDPOINT ='https://develop.adit.qo.is/api';
+    public static API_ENDPOINT ='https://develop.adit.qo.is/api';
     // production
-    public static API_ENDPOINT ='https://adit.qo.is/api';
+    //public static API_ENDPOINT ='https://adit.qo.is/api';
 }
