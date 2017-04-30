b3f72e9cdee639a0422089dd6877ed12042e1d3c
Fabian Hauser <fabian@fh2.ch>
2017-04-30 21:27:45 +0200

    Migrate AdvertisementSearchService API Calls to CallApiService

--- src/app/_services/advertisement-search.service.ts
+++ src/app/_services/advertisement-search.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Http }       from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-
 import 'rxjs/add/operator/map';
 
-import { AppSettings } from '../app.settings';
-import { Advertisement }           from '../data-classes/advertisement';
+import { ApiCallService } from '../utils/api-call.service';
+import { Advertisement } from '../data-classes/advertisement';
 import { Category } from "../data-classes/category";
 import { Tag } from "../data-classes/tag";
 import { SearchProposal } from "../data-classes/search-proposal";
@@ -13,21 +11,18 @@ import { SearchProposal } from "../data-classes/search-proposal";
 @Injectable()
 export class AdvertisementSearchService {
 
-  private apiUrlAdvertisements = AppSettings.API_ENDPOINT + '/advertisements';
-  private apiUrlTags = AppSettings.API_ENDPOINT + '/tags';
-  private apiUrlCategories = AppSettings.API_ENDPOINT + '/categories';
-
-  constructor(private http: Http) {
-  }
+  constructor(private apiCall: ApiCallService) { }
 
   search(term: string): Observable<SearchProposal[]> {
 
-    let advertisementsRequest = this.http.get(`${this.apiUrlAdvertisements}/?title=${term}&description=${term}`).map(res => res.json() as Advertisement[]);
-    let tagsRequest = this.http.get(`${this.apiUrlTags}/?name=${term}`).map(res => res.json() as Tag[]);
-    let categoriesRequest = this.http.get(`${this.apiUrlCategories}/?name=${term}`).map(res => res.json() as Category[]);
+    let advertisementsRequest = this.apiCall.get(`advertisements/?title=${term}&description=${term}`)
+      .map(res => res as Advertisement[]);
+    let tagsRequest = this.apiCall.get(`tags/?name=${term}`)
+      .map(res => res as Tag[]);
+    let categoriesRequest = this.apiCall.get(`categories/?name=${term}`)
+      .map(res => res as Category[]);
 
     return Observable.forkJoin([advertisementsRequest, tagsRequest, categoriesRequest]);
-
   }
 
 }
