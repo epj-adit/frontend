fb8e510b78af70031b86fb60b4460702574c1669
Muriele Trentini <muriele@trentini.ch>
2017-05-03 14:30:54 +0200

    minor changes

--- src/app/advertisementinfo/advertisement-info.component.ts
+++ src/app/advertisementinfo/advertisement-info.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Location }               from '@angular/common';
 
 import { AdvertisementService } from '../_services/advertisement.service';
 import { Advertisement } from '../data-classes/advertisement';
@@ -9,15 +8,14 @@ import { Advertisement } from '../data-classes/advertisement';
   selector: 'adit-advertisementinfo',
   templateUrl: './advertisement-info.component.html',
   styleUrls: ['./advertisement-info.component.scss'],
-  providers: [AdvertisementService]
+  //providers: [AdvertisementService]
 })
 export class AdvertisementInfoComponent implements OnInit {
   advertisement: Advertisement;
 
   constructor(
     private advertisementService: AdvertisementService,
-    private route: ActivatedRoute,
-    private location: Location
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
