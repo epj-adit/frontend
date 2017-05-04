b7e13684a61f3b256781b9554058b91748b85aaf
Fabian Hauser <fabian@fh2.ch>
2017-05-04 11:45:26 +0200

    Rename UserAdvertisementsInfo folder to match file names

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,7 @@ import { AdvertisementListComponent } from './components/advertisement-list/adve
 import { AdvertisementInfoComponent } from './components/advertisement-info/advertisement-info.component';
 import { AdvertisementComponent } from './components/advertisement/advertisement.component';
 import { RegisterComponent } from "./components/register/register.component";
-import { UserAdvertisementsComponent } from "./components/useradvertisements/user-advertisements.component";
+import { UserAdvertisementsComponent } from "./components/user-advertisements/user-advertisements.component";
 import { UserProfileComponent } from "./components/user-profile/user-profile.component";
 import { AccountComponent } from "./components/account/account.component";
 import { SupervisorPanelComponent } from "./components/supervisorpanel/supervisorpanel.component";
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,7 +6,7 @@ import { AdvertisementInfoComponent } from './components/advertisement-info/adve
 import { AdvertisementComponent } from './components/advertisement/advertisement.component';
 import { AdvertisementListComponent } from "./components/advertisement-list/advertisement-list.component";
 import { RegisterComponent } from "./components/register/register.component";
-import { UserAdvertisementsComponent } from "./components/useradvertisements/user-advertisements.component";
+import { UserAdvertisementsComponent } from "./components/user-advertisements/user-advertisements.component";
 import { UserProfileComponent } from "./components/user-profile/user-profile.component";
 import { AccountComponent } from "./components/account/account.component";
 import { SupervisorPanelComponent } from "./components/supervisorpanel/supervisorpanel.component";
