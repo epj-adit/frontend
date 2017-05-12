b5f448420c1f7793e926c8908335af2904b13a25
Muriele Trentini <muriele@trentini.ch>
2017-05-12 10:56:18 +0200

    add tests for search component

--- tests/app/_mocks/advertisement-search-service-stub.ts
+++ tests/app/_mocks/advertisement-search-service-stub.ts
@@ -1,13 +1,17 @@
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/observable/of';
-import { SearchProposal } from "../../../src/app/data/search-proposal";
+import { getAdvertisementMocks } from "../data/mock-advertisements";
+import { getTagMocks } from "../data/mock-tags";
+import { getCategoriesMocks } from "../data/mock-categories";
 
 @Injectable()
 export class AdvertisementSearchServiceStub {
-  search(term: string): Observable<SearchProposal[]> {
-    let arr = [];
-    arr.push(new SearchProposal());
-    return Observable.of(arr);
-  }
+    search(term: string): Observable<any[]> {
+        let arr = [];
+        arr[0]= getAdvertisementMocks();
+        arr[1]= getTagMocks();
+        arr[2]= getCategoriesMocks();
+        return Observable.of(arr);
+    }
 }
--- tests/app/components/advertisement-search.spec.ts
+++ tests/app/components/advertisement-search.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -7,20 +7,16 @@ import { Router, Routes } from "@angular/router";
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 import { HttpModule } from "@angular/http";
 import { Angular2FontawesomeModule } from "angular2-fontawesome";
-import { AdvertisementComponent } from "../../../src/app/components/advertisement/advertisement.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AdvertisementService } from "../../../src/app/services/advertisement.service";
-import { CategoryService } from "../../../src/app/services/category.service";
-import { Tag } from "../../../src/app/data/tag";
 import { FakeTranslationLoader } from "../_mocks/fake-translation-loader";
-import { CategoryServiceStub } from "../_mocks/category-service-stub";
-import { AdvertisementServiceStub } from "../_mocks/advertisement-service-stub";
 import { AdvertisementSearchComponent } from "../../../src/app/components/search/advertisement-search.component";
 import { AdvertisementSearchService } from "../../../src/app/services/advertisement-search.service";
 import { AdvertisementSearchServiceStub } from "../_mocks/advertisement-search-service-stub";
 import { RouterStub } from "../_mocks/router-stub";
 import { ApiCallServiceStub } from "../_mocks/api-call-service-stub";
 import { ApiCallService } from "../../../src/app/utils/api-call.service";
+import { ProposalType, SearchProposal } from "../../../src/app/data/search-proposal";
+import { getAdvertisementMocks } from "../data/mock-advertisements";
 
 
 describe('AdvertisementSearchComponent', () => {
@@ -29,6 +25,8 @@ describe('AdvertisementSearchComponent', () => {
     ];
     let comp: AdvertisementSearchComponent;
     let fixture: ComponentFixture<AdvertisementSearchComponent>;
+    let searchService: AdvertisementSearchService;
+    let router: Router;
     let de: DebugElement;
     let el: HTMLElement;
 
@@ -57,11 +55,81 @@ describe('AdvertisementSearchComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(AdvertisementSearchComponent);
         comp = fixture.componentInstance;
+        searchService = TestBed.get(AdvertisementSearchService);
+        router = TestBed.get(Router);
     });
 
     it('should not have an inital searchProposal', () => {
         expect(comp.searchProposals).toBeUndefined();
     });
 
+    describe('after ngOnInit', () => {
+        beforeEach(async(() => {
+            fixture.detectChanges();
+            fixture.whenStable()
+                .then(() => fixture.detectChanges());
+        }));
 
+        it('should have a searchProposal', () => {
+            expect(comp.searchProposals).toBeDefined();
+        });
+
+        it('should search keyed in terms', fakeAsync(() => {
+            spyOn(comp, 'search').and.callThrough();
+            spyOn(comp, 'addToProposals').and.callFake(
+                () => {
+                    let ad = getAdvertisementMocks()[0];
+                    let proposals: SearchProposal[] = [];
+                    console.log("addtoproposal");
+                    proposals.push(
+                        {
+                            id: ad.id,
+                            name: ad.title,
+                            type: ProposalType.Advert,
+                            displayName: ProposalType[ProposalType.Advert]
+                        }
+                    );
+                    return proposals;
+                }
+            );
+            let searchString = 'test';
+            comp.search(searchString);
+            de = fixture.debugElement.query(By.css('#search-box'));
+            el = de.nativeElement;
+            (<HTMLInputElement>el).value = searchString;
+            tick(300);
+            expect(comp.search).toHaveBeenCalledWith(searchString);
+        }));
+
+        describe('gotoInfo', ()=> {
+            it('should route to advertisementinfo for advertisements', () => {
+                spyOn(router, 'navigate');
+                let sp = new SearchProposal();
+                sp.type = ProposalType.Advert;
+                sp.id = 1;
+                let link = ['/advertisementinfo', sp.id];
+                comp.gotoInfo(sp);
+                expect(router.navigate).toHaveBeenCalledWith(link);
+            });
+            it('should route to advertisements with tagfilter for tags', () => {
+                spyOn(router, 'navigate');
+                let sp = new SearchProposal();
+                sp.type = ProposalType.Tag;
+                sp.id = 1;
+                let link = ['advertisements', {tagId: sp.id}];
+                comp.gotoInfo(sp);
+                expect(router.navigate).toHaveBeenCalledWith(link);
+            });
+            it('should route to advertisements with categoryfilter for categories', () => {
+                spyOn(router, 'navigate');
+                let sp = new SearchProposal();
+                sp.type = ProposalType.Category;
+                sp.id = 1;
+                let link = ['advertisements', {categoryId: sp.id}];
+                comp.gotoInfo(sp);
+                expect(router.navigate).toHaveBeenCalledWith(link);
+            });
+        });
+
+    });
 });
