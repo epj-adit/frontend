0723003f06e1dc397fa309eb76cb89d2a561a545
Muriele Trentini <muriele@trentini.ch>
2017-05-22 15:58:30 +0200

    add filter to only show users advertisements in useradvertisements

--- src/app/components/user-advertisements/user-advertisements.component.ts
+++ src/app/components/user-advertisements/user-advertisements.component.ts
@@ -7,57 +7,61 @@ import { Advertisement } from '../../data/advertisement';
 import { AdvertisementService } from '../../services/advertisement.service';
 import { TranslateService } from "@ngx-translate/core";
 import { Observable } from "rxjs/Observable";
+import { AuthenticationService } from "../../utils/authentication.service";
+import { userInfo } from "os";
 
 @Component({
-  selector: 'adit-useradvertisements',
-  templateUrl: './user-advertisements.component.html',
-  styleUrls: ['./user-advertisements.component.scss']
+    selector: 'adit-useradvertisements',
+    templateUrl: './user-advertisements.component.html',
+    styleUrls: ['./user-advertisements.component.scss']
 })
 export class UserAdvertisementsComponent implements OnInit {
-  advertisements: Advertisement[];
+    advertisements: Advertisement[];
+    userId: number;
 
-  constructor(private router: Router,
-              private advertisementService: AdvertisementService,
-              overlay: Overlay, vcRef: ViewContainerRef, public modal: Modal,
-              private translate: TranslateService) {
-    overlay.defaultViewContainer = vcRef;
-  }
+    constructor(private router: Router,
+                private advertisementService: AdvertisementService,
+                overlay: Overlay, vcRef: ViewContainerRef, public modal: Modal,
+                private translate: TranslateService,
+                private authenticationService: AuthenticationService) {
+        authenticationService.getUser().subscribe(user => this.userId = user.id);
+        overlay.defaultViewContainer = vcRef;
+    }
 
-  getAdvertisements(): void {
-    this.advertisementService.getAdvertisements().subscribe(advertisements => this.advertisements = advertisements);
-  }
+    getAdvertisements(): void {
+        this.advertisementService.getAdvertisementsQuery('/?userId=' + this.userId).subscribe(advertisements => this.advertisements = advertisements);
+    }
 
-  edit(advertisement: Advertisement): void {
-    this.advertisementService.currentAdvertisement = advertisement;
-    let link = ['/advertisement', advertisement.id];
-    this.router.navigate(link);
-  }
+    edit(advertisement: Advertisement): void {
+        this.advertisementService.currentAdvertisement = advertisement;
+        let link = ['/advertisement', advertisement.id];
+        this.router.navigate(link);
+    }
 
-  deleteAd(advertisement: Advertisement): void {
-    let translateTitle = this.translate.get('USERADVERTISEMENT.deleteConfirmTitle');
-    let translateDialog = this.translate.get('USERADVERTISEMENT.deleteConfirmDialog');
-    Observable.forkJoin([translateTitle, translateDialog]).subscribe(
-      res => this.modal.confirm()
-        .size('sm')
-        .showClose(true)
-        .keyboard(27)
-        .title(res[0])
-        .body(res[1])
-        .okBtnClass('btn btn-danger')
-        .cancelBtnClass('btn btn-default')
-        .open()
-        .then((resultPromise) => {
-          return resultPromise.result.then(
-            (result) => {
-              if (result) this.advertisementService.deleteAd(advertisement).subscribe();
-            }, () => console.log('Delete action rejected by user.'));
-        })
+    deleteAd(advertisement: Advertisement): void {
+        let translateTitle = this.translate.get('USERADVERTISEMENT.deleteConfirmTitle');
+        let translateDialog = this.translate.get('USERADVERTISEMENT.deleteConfirmDialog');
+        Observable.forkJoin([translateTitle, translateDialog]).subscribe(
+            res => this.modal.confirm()
+                .size('sm')
+                .showClose(true)
+                .keyboard(27)
+                .title(res[0])
+                .body(res[1])
+                .okBtnClass('btn btn-danger')
+                .cancelBtnClass('btn btn-default')
+                .open()
+                .then((resultPromise) => {
+                    return resultPromise.result.then(
+                        (result) => {
+                            if (result) this.advertisementService.deleteAd(advertisement).subscribe();
+                        }, () => console.log('Delete action rejected by user.'));
+                })
+        );
+    }
 
-    );
-  }
-
-  ngOnInit(): void {
-    this.getAdvertisements();
-  }
+    ngOnInit(): void {
+        this.getAdvertisements();
+    }
 
 }
